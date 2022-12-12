69f4e81f35bfb7319793b8438634923378481125
mariethaasnat <112157528+mariethaasnat@users.noreply.github.com>
2022-12-12 22:45:59 +0800

    Fix import

--- lib/apps/project/card.dart
+++ lib/apps/project/card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pedulee/models/project.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class ProjectCard extends StatelessWidget {
   final String title;
