38b4d7677c933fe2081f070bdcb82703a0d7dd51
Ghayda Rafa Hernawan <ghayda.rafa@gmail.com>
2022-12-12 11:30:06 +0700

    deploy apk

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -22,8 +22,8 @@ echo "$KEY_JKS" | base64 --decode > release-keystore.jks
 
 # build APK
 # if you get "Execution failed for task ':app:lintVitalRelease'." error, uncomment next two lines
-# flutter build apk --debug
-# flutter build apk --profile
+flutter build apk --debug
+flutter build apk --profile
 flutter build apk --release
 
 # if you need build bundle (AAB) in addition to your APK, uncomment line below and last line of this script.
